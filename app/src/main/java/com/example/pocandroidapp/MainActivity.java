07ae9a410f4571aaa2f639056d5b8e8147cdc575
tsinemkorkmaz <tsinemkorkmaz@epias.com.tr>
2023-09-12 18:31:15 +0300

    added login page

--- app/src/main/java/com/example/pocandroidapp/MainActivity.java
+++ app/src/main/java/com/example/pocandroidapp/MainActivity.java
@@ -1,45 +1,33 @@
 package com.example.pocandroidapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("kloia") && password.getText().toString().equals("kloia1234")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    @SuppressLint("SetTextI18n")
-    public void onClickIndia(View view) {
-        String formattedDate = getTime("Asia/Kolkata");
-        TextView textView = findViewById(R.id.my_clock);
-        textView.setText("India: " + formattedDate);
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void onClickGermany(View view) {
-        String formattedDate = getTime("Europe/Berlin");
-        TextView textView = findViewById(R.id.my_clock);
-        textView.setText("Germany: " + formattedDate);
-    }
-
-    public String getTime(String timeZoneParam){
-        TimeZone timeZone = TimeZone.getTimeZone(timeZoneParam);
-        Calendar calendar = Calendar.getInstance(timeZone);
-
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        sdf.setTimeZone(timeZone);
-
-        return  sdf.format(calendar.getTime());
-    }
 }
